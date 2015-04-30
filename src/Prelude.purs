5b90e700682f948dd3a908d36b3feead7bf010bc
Harry Garrood <harry@garrood.me>
2015-04-30 19:33:15 +0100

    Implement ordArray without cons binders

--- src/Prelude.purs
+++ src/Prelude.purs
@@ -787,12 +787,42 @@ instance ordUnit :: Ord Unit where
   compare _ _ = EQ
 
 instance ordArray :: (Ord a) => Ord (Array a) where
-  compare [] [] = EQ
-  compare [] _ = LT
-  compare _ [] = GT
-  compare (x:xs) (y:ys) = case compare x y of
-    EQ -> compare xs ys
-    other -> other
+  compare = compareArrayImpl checkEqOr EQ
+    where
+    checkEqOr a b callback =
+      case compare a b of
+        EQ -> unit
+        other -> callback other
+
+foreign import compareArrayImpl
+  """
+  function compareArrayImpl(checkEqOr) {
+    return function(eq) {
+      return function(xs) {
+        return function(ys) {
+          var minlen = xs.length
+          var maxlen = ys.length
+          var result = null
+
+          if (maxlen < minlen) {
+             var tmp = minlen
+             minlen = maxlen
+             maxlen = tmp
+          }
+
+          for (var i = 0; i < minlen && result === null; ++i) {
+            checkEqOr(xs[i])(ys[i])(function(other) { result = other })
+          }
+
+          if (result === null) {
+             result = eq
+           }
+          return result
+        }
+      }
+    }
+  }
+  """ :: forall a. (a -> a -> (Ordering -> Unit) -> Unit) -> Ordering -> Array a -> Array a -> Ordering
 
 instance ordOrdering :: Ord Ordering where
   compare LT LT = EQ
