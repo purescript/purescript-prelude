8b5dd4367d0b8c63eb6e0810763488984aad3a85
Michael Ficarra <git@michael.ficarra.me>
2016-01-18 00:38:13 -0800

    fixes #51: properly escape output of Show instance for Char

--- src/Prelude.js
+++ src/Prelude.js
@@ -210,7 +210,20 @@ exports.showNumberImpl = function (n) {
 };
 
 exports.showCharImpl = function (c) {
-  return c === "'" ? "'\\''" : "'" + c + "'";
+  var code = c.charCodeAt(0);
+  if (code < 0x20 || code === 0x7F) {
+    switch (c) {
+      case "\a": return "'\\a'";
+      case "\b": return "'\\b'";
+      case "\f": return "'\\f'";
+      case "\n": return "'\\n'";
+      case "\r": return "'\\r'";
+      case "\t": return "'\\t'";
+      case "\v": return "'\\v'";
+    }
+    return "'\\" + code.toString(10) + "'";
+  }
+  return c === "'" || c === "\\" ? "'\\" + c + "'" : "'" + c + "'";
 };
 
 exports.showStringImpl = function (s) {
