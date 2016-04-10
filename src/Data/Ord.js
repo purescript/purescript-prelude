6aae6d6b876c88327ce8ef31dbf3dec8eb60d3a4
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2016-04-10 19:12:09 +0100

    Remove unused (moved) FFI function

--- src/Data/Ord.js
+++ src/Data/Ord.js
@@ -27,15 +27,3 @@ exports.ordArrayImpl = function (f) {
     };
   };
 };
-
-exports.unsafeCompareImpl = function (lt) {
-  return function (eq) {
-    return function (gt) {
-      return function (x) {
-        return function (y) {
-          return x < y ? lt : x > y ? gt : eq;
-        };
-      };
-    };
-  };
-};
