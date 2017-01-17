f57341554502e2ecee016b549953bab46e470d87
Alex Berg <chexxor@users.noreply.github.com>
2017-01-17 15:36:24 -0600

    Add short motivation of Void type

--- src/Data/Void.purs
+++ src/Data/Void.purs
@@ -2,6 +2,11 @@ module Data.Void (Void, absurd) where
 
 import Data.Show (class Show)
 
+-- | An uninhabited data type.
+-- |
+-- | `Void` is useful to eliminate the possibility of a value being created.
+-- | For example, a value of type `Either Void Boolean` can never have
+-- | a Left value created in PureScript.
 newtype Void = Void Void
 
 instance showVoid :: Show Void where
