aa50747feb60e182a934bda6407eec0a21fbf4aa
Harry Garrood <harry@garrood.me>
2017-01-29 21:59:55 +0000

    Add Bind laws

---
+++ src/Control/Bind/Laws.purs
@@ -0,0 +1,12 @@
+module Control.Bind.Laws where
+
+import Control.Bind
+import Data.Eq
+
+-- | Checks the associativity law for a given triple of values:
+-- |
+-- |      (x >>= f) >>= g = x >>= (\k -> f k >>= g)
+associativity :: forall m a b c. (Bind m, Eq (m c)) => m a -> (a -> m b) -> (b -> m c) -> Boolean
+associativity x f g =
+  ((x >>= f) >>= g) == (x >>= (\k -> f k >>= g))
+
