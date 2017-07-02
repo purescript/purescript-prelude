cd0e77cdcb9eaf2a704ce4f426cd3ad7d2dd52c2
Braxton <bpspenc2@illinois.edu>
2017-07-02 16:57:14 -0500

    Fix minor typo in documentation

--- src/Control/Applicative.purs
+++ src/Control/Applicative.purs
@@ -53,12 +53,12 @@ instance applicativeArray :: Applicative Array where
 liftA1 :: forall f a b. Applicative f => (a -> b) -> f a -> f b
 liftA1 f a = pure f <*> a
 
--- | Perform a applicative action when a condition is true.
+-- | Perform an applicative action when a condition is true.
 when :: forall m. Applicative m => Boolean -> m Unit -> m Unit
 when true m = m
 when false _ = pure unit
 
--- | Perform a applicative action unless a condition is true.
+-- | Perform an applicative action unless a condition is true.
 unless :: forall m. Applicative m => Boolean -> m Unit -> m Unit
 unless false m = m
 unless true _ = pure unit
