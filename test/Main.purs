530077098b5bf0789992ed5817f9b661a2495d24
Gary Burgess <gary.burgess@gmail.com>
2017-08-05 16:14:34 +0100

    Add enum tests, convert existing "tests" into assertions

--- test/Main.purs
+++ test/Main.purs
@@ -1,13 +1,18 @@
 module Test.Main where
 
 import Prelude
+
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Control.Monad.Eff.Console (CONSOLE, log, logShow)
+import Data.Enum (class BoundedEnum, class Enum, Cardinality(..), cardinality, fromEnum, pred, succ, toEnum)
 import Data.Generic.Rep as G
+import Data.Generic.Rep.Bounded as GBounded
+import Data.Generic.Rep.Enum as GEnum
 import Data.Generic.Rep.Eq as GEq
 import Data.Generic.Rep.Ord as GOrd
 import Data.Generic.Rep.Show as GShow
-import Data.Generic.Rep.Bounded as GBounded
+import Data.Maybe (Maybe(..))
+import Test.Assert (ASSERT, assert)
 
 data List a = Nil | Cons { head :: a, tail :: List a }
 
@@ -36,16 +41,98 @@ instance showSimpleBounded :: Show SimpleBounded where
 instance boundedSimpleBounded :: Bounded SimpleBounded where
   bottom = GBounded.genericBottom
   top = GBounded.genericTop
+instance enumSimpleBounded :: Enum SimpleBounded where
+  pred = GEnum.genericPred
+  succ = GEnum.genericSucc
+instance boundedEnumSimpleBounded :: BoundedEnum SimpleBounded where
+  cardinality = GEnum.genericCardinality
+  toEnum = GEnum.genericToEnum
+  fromEnum = GEnum.genericFromEnum
 
-main :: Eff (console :: CONSOLE) Unit
+data Option a = None | Some a
+derive instance genericOption :: G.Generic (Option a) _
+instance eqOption :: Eq a => Eq (Option a) where
+  eq x y = GEq.genericEq x y
+instance ordOption :: Ord a => Ord (Option a) where
+  compare x y = GOrd.genericCompare x y
+instance showOption :: Show a => Show (Option a) where
+  show x = GShow.genericShow x
+instance boundedOption :: Bounded a => Bounded (Option a) where
+  bottom = GBounded.genericBottom
+  top = GBounded.genericTop
+instance enumOption :: (Bounded a, Enum a) => Enum (Option a) where
+  pred = GEnum.genericPred
+  succ = GEnum.genericSucc
+instance boundedEnumOption :: BoundedEnum a => BoundedEnum (Option a) where
+  cardinality = GEnum.genericCardinality
+  toEnum = GEnum.genericToEnum
+  fromEnum = GEnum.genericFromEnum
+
+main :: Eff (console :: CONSOLE, assert :: ASSERT) Unit
 main = do
   logShow (cons 1 (cons 2 Nil))
 
-  logShow (cons 1 (cons 2 Nil) == cons 1 (cons 2 Nil))
-  logShow (cons 1 (cons 2 Nil) == cons 1 Nil)
+  log "Checking equality"
+  assert $ cons 1 (cons 2 Nil) == cons 1 (cons 2 Nil)
 
-  logShow (cons 1 (cons 2 Nil) `compare` cons 1 (cons 2 Nil))
-  logShow (cons 1 (cons 2 Nil) `compare` cons 1 Nil)
+  log "Checking inequality"
+  assert $ cons 1 (cons 2 Nil) /= cons 1 Nil
 
-  logShow (bottom :: SimpleBounded)
-  logShow (top :: SimpleBounded)
+  log "Checking comparison EQ"
+  assert $ (cons 1 (cons 2 Nil) `compare` cons 1 (cons 2 Nil)) == EQ
+
+  log "Checking comparison GT"
+  assert $ (cons 1 (cons 2 Nil) `compare` cons 1 Nil) == GT
+
+  log "Checking comparison LT"
+  assert $ (cons 1 Nil `compare` cons 1 (cons 2 Nil)) == LT
+
+  log "Checking simple bottom"
+  assert $ bottom == A
+
+  log "Checking simple top"
+  assert $ top == D
+
+  log "Checking composite bottom"
+  assert $ bottom == None :: Option SimpleBounded
+
+  log "Checking composite top"
+  assert $ top == Some D
+
+  log "Checking simple pred bottom"
+  assert $ pred (bottom :: SimpleBounded) == Nothing
+
+  log "Checking simple (pred =<< succ bottom)"
+  assert $ (pred =<< succ bottom) == Just A
+
+  log "Checking simple succ top"
+  assert $ succ (top :: SimpleBounded) == Nothing
+
+  log "Checking simple (succ =<< pred top)"
+  assert $ (succ =<< pred top) == Just D
+
+  log "Checking composite pred bottom"
+  assert $ pred (bottom :: Option SimpleBounded) == Nothing
+
+  log "Checking composite (pred =<< succ bottom)"
+  assert $ (pred =<< succ (bottom :: Option SimpleBounded)) == Just None
+
+  log "Checking composite succ top"
+  assert $ succ (top :: Option SimpleBounded) == Nothing
+
+  log "Checking composite (succ =<< pred top)"
+  assert $ (succ =<< pred top) == Just (Some D)
+
+  log "Checking simple cardinality"
+  assert $ (cardinality :: Cardinality SimpleBounded) == Cardinality 4
+
+  log "Checking composite cardinality"
+  assert $ (cardinality :: Cardinality (Option SimpleBounded)) == Cardinality 5
+
+  log "Checking simple toEnum/fromEnum roundtrip"
+  assert $ toEnum (fromEnum A) == Just A
+  assert $ toEnum (fromEnum B) == Just B
+
+  log "Checking composite toEnum/fromEnum roundtrip"
+  assert $ toEnum (fromEnum (None :: Option SimpleBounded)) == Just (None :: Option SimpleBounded)
+  assert $ toEnum (fromEnum (Some A)) == Just (Some A)
