2b8f97624c9f09e5147ff78008671e570251fe90
rightfold <rightfold@gmail.com>
2017-08-26 22:28:43 +0200

    Remove RightModule and rename LeftModule to Module

--- src/Data/Module.purs
+++ src/Data/Module.purs
@@ -3,7 +3,7 @@ module Data.Module where
 import Data.Ring (class Ring, zero, add, sub, mul)
 import Data.Unit (Unit, unit)
 
--- | The `LeftModule` class is for types that support addition as well as
+-- | The `Module` class is for types that support addition as well as
 -- | multiplication by a scalar on the left.
 -- |
 -- | If you want a vector space, simply specify an additional `Field s`
@@ -17,80 +17,34 @@ import Data.Unit (Unit, unit)
 -- |   - Commutative: `a $+ b = b $+ a`
 -- | - Multiplicative identity: `one $* a = a`
 -- | - Multiplication distributes over addition:
--- |   - Left distributivity: `a $* (b $+ c) = (a $* b) $+ (a $* c)`
+-- |   -  distributivity: `a $* (b $+ c) = (a $* b) $+ (a $* c)`
 -- |   - Right distributivity: `(a + b) $* c = (a $* c) $+ (b $* c)`
 -- | - Annihilation: `zero $* a = mlzero`
 -- | - Compatibility: `(a * b) $* c = a $* (b $* c)`
-class (Ring s) <= LeftModule m s | m -> s where
-  mlzero :: m
-  mladd :: m -> m -> m
-  mlsub :: m -> m -> m
-  mlmul :: s -> m -> m
+class (Ring s) <= Module m s | m -> s where
+  mzero :: m
+  madd :: m -> m -> m
+  msub :: m -> m -> m
+  mmul :: s -> m -> m
 
-infixl 6 mladd as $+
-infixl 6 mlsub as $-
-infixl 7 mlmul as $*
+infixl 6 madd as $+
+infixl 6 msub as $-
+infixl 7 mmul as $*
 
--- | The `RightModule` class is for types that support addition as well as
--- | multiplication by a scalar on the right.
--- |
--- | If you want a vector space, simply specify an additional `Field s`
--- | constraint.
--- |
--- | Instances must satisfy the following laws in addition to the `Ring` laws:
--- |
--- | - Commutative monoid under addition:
--- |   - Associativity: `(a +$ b) +$ c = a +$ (b +$ c)`
--- |   - Identity: `mlzero +$ a = a +$ mlzero = a`
--- |   - Commutative: `a +$ b = b +$ a`
--- | - Multiplicative identity: `a *$ one = a`
--- | - Multiplication distributes over addition:
--- |   - Left distributivity: `a *$ (a + b) = (a *$ c) +$ (b *$ c)`
--- |   - Right distributivity: `(a +$ b) *$ c = (a *$ c) +$ (b *$ c)`
--- | - Annihilation: `zero *$ a = mlzero`
--- | - Compatibility: `a *$ (b * c) = (a *$ b) *$ c`
-class (Ring s) <= RightModule m s | m -> s where
-  mrzero :: m
-  mradd :: m -> m -> m
-  mrsub :: m -> m -> m
-  mrmul :: m -> s -> m
+instance moduleUnit :: (Ring s) => Module Unit s where
+  mzero = unit
+  madd _ _ = unit
+  msub _ _ = unit
+  mmul _ _ = unit
 
-infixl 6 mradd as +$
-infixl 6 mrsub as -$
-infixl 7 mrmul as *$
+instance moduleInt :: Module Int Int where
+  mzero = zero
+  madd = add
+  msub = sub
+  mmul = mul
 
-instance leftModuleUnit :: (Ring s) => LeftModule Unit s where
-  mlzero = unit
-  mladd _ _ = unit
-  mlsub _ _ = unit
-  mlmul _ _ = unit
-
-instance rightModuleUnit :: (Ring s) => RightModule Unit s where
-  mrzero = unit
-  mradd _ _ = unit
-  mrsub _ _ = unit
-  mrmul _ _ = unit
-
-instance leftModuleInt :: LeftModule Int Int where
-  mlzero = zero
-  mladd = add
-  mlsub = sub
-  mlmul = mul
-
-instance rightModuleInt :: RightModule Int Int where
-  mrzero = zero
-  mradd = add
-  mrsub = sub
-  mrmul = mul
-
-instance leftModuleNumber :: LeftModule Number Number where
-  mlzero = zero
-  mladd = add
-  mlsub = sub
-  mlmul = mul
-
-instance rightModuleNumber :: RightModule Number Number where
-  mrzero = zero
-  mradd = add
-  mrsub = sub
-  mrmul = mul
+instance moduleNumber :: Module Number Number where
+  mzero = zero
+  madd = add
+  msub = sub
+  mmul = mul
