49cf134612d37e9d735196b82103c5b9fb9aea2c
rightfold <rightfold@gmail.com>
2017-08-26 23:17:44 +0200

    Make Module require a CommutativeRing

--- src/Data/Module.purs
+++ src/Data/Module.purs
@@ -1,6 +1,7 @@
 module Data.Module where
 
-import Data.Ring (class Ring, zero, add, sub, mul)
+import Data.Ring (zero, add, sub, mul)
+import Data.CommutativeRing (class CommutativeRing)
 import Data.Unit (Unit, unit)
 
 -- | The `Module` class is for types that support addition as well as
@@ -9,7 +10,8 @@ import Data.Unit (Unit, unit)
 -- | If you want a vector space, simply specify an additional `Field s`
 -- | constraint.
 -- |
--- | Instances must satisfy the following laws in addition to the `Ring` laws:
+-- | Instances must satisfy the following laws in addition to the
+-- | `CommutativeRing` laws:
 -- |
 -- | - Commutative monoid under addition:
 -- |   - Associativity: `(a $+ b) $+ c = a $+ (b $+ c)`
@@ -17,11 +19,11 @@ import Data.Unit (Unit, unit)
 -- |   - Commutative: `a $+ b = b $+ a`
 -- | - Multiplicative identity: `one $* a = a`
 -- | - Multiplication distributes over addition:
--- |   -  distributivity: `a $* (b $+ c) = (a $* b) $+ (a $* c)`
+-- |   - Left distributivity: `a $* (b $+ c) = (a $* b) $+ (a $* c)`
 -- |   - Right distributivity: `(a + b) $* c = (a $* c) $+ (b $* c)`
 -- | - Annihilation: `zero $* a = mlzero`
 -- | - Compatibility: `(a * b) $* c = a $* (b $* c)`
-class (Ring s) <= Module m s | m -> s where
+class (CommutativeRing s) <= Module m s | m -> s where
   mzero :: m
   madd :: m -> m -> m
   msub :: m -> m -> m
@@ -31,7 +33,17 @@ infixl 6 madd as $+
 infixl 6 msub as $-
 infixl 7 mmul as $*
 
-instance moduleUnit :: (Ring s) => Module Unit s where
+-- | `mmulFlipped` is `mmul` with the arguments flipped. For example:
+-- |
+-- | ```purescript
+-- | twiceAsLong = (_ *$ 2.0)
+-- | ```
+mmulFlipped :: forall m s. Module m s => m -> s -> m
+mmulFlipped a b = b $* a
+
+infixl 7 mmulFlipped as *$
+
+instance moduleUnit :: (CommutativeRing s) => Module Unit s where
   mzero = unit
   madd _ _ = unit
   msub _ _ = unit
