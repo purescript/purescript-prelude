95ad0bdf18b31cdb97e68c06ceafc5f8ac5b2ebf
Eric Brisco <eric.brisco@gmail.com>
2018-03-24 16:04:03 -0400

    ADD: Bounded Number instance.

--- src/Data/Bounded.js
+++ src/Data/Bounded.js
@@ -5,3 +5,6 @@ exports.bottomInt = -2147483648;
 
 exports.topChar = String.fromCharCode(65535);
 exports.bottomChar = String.fromCharCode(0);
+
+exports.topNumber = Number.POSITIVE_INFINITY;
+exports.bottomNumber = Number.NEGATIVE_INFINITY;
