816fd1cb56dd7b10afa2e42e504c02915e6df41b
Profpatsch <mail@profpatsch.de>
2018-07-19 21:09:19 +0200

    Data.Eq: add a disclaimer for eqArrayImpl shortcut
    Based on the discussion in
    https://github.com/purescript/purescript-prelude/commit/3b0d306c0fa20af4803fdad2313f250d4e388d35#commitcomment-29689735

--- src/Data/Eq.js
+++ src/Data/Eq.js
@@ -9,7 +9,13 @@ exports.refEq = function (r1) {
 exports.eqArrayImpl = function (f) {
   return function (xs) {
     return function (ys) {
+      
+      /* disclaimer: this check might break referential equality
+         for non-lawful instances of `Eq` (e.g. `eq _ _ = false`).
+         We strongly discourage unlawful instances, so it shouldn't
+         be a problem in practice. */
       if (xs === ys) return true;
+
       if (xs.length !== ys.length) return false;
       for (var i = 0; i < xs.length; i++) {
         if (!f(xs[i])(ys[i])) return false;
