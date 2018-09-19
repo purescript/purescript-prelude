23c0fce20636022f5826677aa125181f7501c7bd
Liam Goodacre <goodacre.liam@gmail.com>
2018-09-19 21:28:02 +0100

    Remove the ref equality check in the Eq Array instance
    See discussion here: https://github.com/purescript/purescript-prelude/commit/3b0d306c0fa20af4803fdad2313f250d4e388d35

--- src/Data/Eq.js
+++ src/Data/Eq.js
@@ -9,7 +9,6 @@ exports.refEq = function (r1) {
 exports.eqArrayImpl = function (f) {
   return function (xs) {
     return function (ys) {
-      if (xs === ys) return true;
       if (xs.length !== ys.length) return false;
       for (var i = 0; i < xs.length; i++) {
         if (!f(xs[i])(ys[i])) return false;
