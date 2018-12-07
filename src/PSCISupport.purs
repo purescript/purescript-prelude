9d807e4c669878d10ec88fb93a342e350271f771
Harry Garrood <harry@garrood.me>
2018-12-07 20:33:11 +0000

    Update PSCI support module to support eval-ing Effect

--- src/PSCISupport.purs
+++ src/PSCISupport.purs
@@ -1,9 +1,24 @@
-module PSCI.Support ( eval ) where
+-- | This module provides support for the
+-- | PureScript interactive mode, PSCI.
+
+module PSCI.Support where
 
 import Prelude
+
 import Effect (Effect)
 import Effect.Console (log)
-import Data.Debugged (class Debug, debugged, prettyPrintOneLine)
+import Data.Debugged (class Debug, debugged, prettyPrint)
 
-eval :: forall a. Debug a => a -> Effect Unit
-eval = log <<< prettyPrintOneLine <<< debugged
+-- | The `Eval` class captures those types which can be
+-- | evaluated in the REPL.
+-- |
+-- | There are instances provided for the `Effect` type
+-- | constructor and any `Debug` types.
+class Eval a where
+  eval :: a -> Effect Unit
+
+instance evalEffect :: Eval a => Eval (Effect a) where
+  eval x = x >>= eval
+else
+instance evalDebug :: Debug a => Eval a where
+  eval = log <<< prettyPrint <<< debugged
