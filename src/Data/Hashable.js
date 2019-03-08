eb3ffa11a7b87776dc97929c812bb2fabb3b11bc
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2019-03-08 15:19:13 +0000

    hashWithSalt

--- src/Data/Hashable.js
+++ src/Data/Hashable.js
@@ -1,42 +1,49 @@
 "use strict";
 
-// Same as immutable.js, except for not dropping the highest bit.
-exports.hashNumber = function (f) {
-  var o = f;
-  if (o !== o || o === Infinity) {
-    return 0;
-  }
-  var h = o | 0;
-  if (h !== o) {
-    h ^= o * 0xffffffff;
-  }
-  while (o > 0xffffffff) {
-    o /= 0xffffffff;
-    h ^= o;
-  }
-  return h;
+// Inspired by immutable.js, except for not dropping the highest bit
+// and taking a seed.
+exports.hashNumber = function (s) {
+  return function (f) {
+    var o = f;
+    if (o !== o || o === Infinity) {
+      return s;
+    }
+    var h = o | 0;
+    if (h !== o) {
+      h ^= o * 0xffffffff;
+    }
+    while (o > 0xffffffff) {
+      o /= 0xffffffff;
+      h ^= o;
+    }
+      return (s+h)|0;
+  };
 };
 
-// Same as Java. Improvements welcome.
-exports.hashString = function (s) {
-  var h = 0;
-  for (var i = 0; i < s.length; i++) {
-    h = (31 * h + s.charCodeAt(i)) | 0;
-  }
-  return h;
-};
-
-// Almost the same as Java. Improvements welcome.
-exports.hashArray = function (hash) {
-  return function (as) {
-    var h = 0;
-    for (var i = 0; i < as.length; i++) {
-      h = (31 * h + hash(as[i])) | 0;
+exports.hashString = function (seed) {
+  return function (s) {
+    var h = s;
+    for (var i = 0; i < s.length; i++) {
+      h = (31 * h + s.charCodeAt(i)) | 0;
     }
     return h;
   };
 };
 
-exports.hashChar = function (c) {
-  return c.charCodeAt(0);
+exports.hashArray = function (hash) {
+  return function (s) {
+    return function (as) {
+      var h = s;
+      for (var i = 0; i < as.length; i++) {
+        h = (31 * h + hash(as[i])) | 0;
+      }
+      return h;
+    };
+  };
+};
+
+exports.hashChar = function (s) {
+  return function (c) {
+    return (s+c.charCodeAt(0))|0;
+  };
 };
--- src/Data/Hashable.purs
+++ src/Data/Hashable.purs
@@ -1,5 +1,6 @@
 module Data.Hashable (
   class Hashable,
+  hashWithSalt,
   hash,
 
   class HashableRecord,
@@ -11,7 +12,7 @@ module Data.Hashable (
 import Data.Eq (class Eq, class EqRecord)
 import Data.Ord (class Ord)
 import Data.Ordering (Ordering(..))
-import Data.Ring (negate)
+import Data.Ring ((-))
 import Data.Semigroup ((<>))
 import Data.Semiring ((*), (+))
 import Data.Show (class Show, show)
@@ -24,31 +25,36 @@ import Record.Unsafe (unsafeGet)
 import Type.Data.RowList (RLProxy(..))
 
 -- | The `Hashable` type class represents types with decidable
--- | equality and a hash function whose result can approximate
+-- | equality and a seeded hash function whose result can approximate
 -- | equality for use in hash-based algorithms and data structures.
 -- |
 -- | Instances of `Hashable` must satisfy the following law:
 -- |
 -- | ```PureScript
--- | (a == b) `implies` (hash a == hash b)
+-- | (a == b) `implies` (hashWithSalt s a == hashWithSalt s b)
 -- | ```
 -- |
 -- | That is, unequal hash values are a safe approximation of
 -- | inequality. In other words, two objects whose hash values differ,
 -- | are never equal. The reverse is not necessarily true.
 -- |
--- | Hash values produced by `hash` must not be relied upon to be
--- | stable across multiple executions of a program and should not be
--- | stored externally.
+-- | Hash values produced by `hashWithSalt` are not cryptographically
+-- | secure, must not be relied upon to be stable across multiple
+-- | executions of a program, and should not be stored externally.
 class Eq a <= Hashable a where
-  hash :: a -> Hash a
+  hashWithSalt :: Int -> a -> Hash a
 
 -- | The `Hash a` newtype wraps the hash code of a value of type `a`.
 -- |
 -- | Hash values should not be stored externally, as they must not be
--- | relied upon to be stable across multiple executions of a
--- | program.
+-- | relied upon to be stable across multiple executions of a program.
 newtype Hash a = Hash Int
+
+-- | A convenience function that calls `hashWithSalt` on its argument
+-- | with some seed. This seed is fixed for the runtime of a program,
+-- | but may change between runs. Do not store hashes externally.
+hash :: forall a. Hashable a => a -> Hash a
+hash = hashWithSalt 42
 
 instance showHash :: Show (Hash a) where
   show (Hash n) = "(Hash " <> show n <> ")"
@@ -56,47 +62,47 @@ derive newtype instance eqHash :: Eq (Hash a)
 derive newtype instance ordHash :: Ord (Hash a)
 
 instance hashableBoolean :: Hashable Boolean where
-  hash b = if b then Hash 1 else Hash 0
+  hashWithSalt s b = Hash (s + if b then 1 else 0)
 
 instance hashableInt :: Hashable Int where
-  hash n = Hash n
+  hashWithSalt s n = Hash (s + n)
 
-foreign import hashNumber :: Number -> Hash Number
+foreign import hashNumber :: Int -> Number -> Hash Number
 
 instance hashableNumber :: Hashable Number where
-  hash = hashNumber
+  hashWithSalt = hashNumber
 
-foreign import hashChar :: Char -> Hash Char
+foreign import hashChar :: Int -> Char -> Hash Char
 
 instance hashableChar :: Hashable Char where
-  hash = hashChar
+  hashWithSalt = hashChar
 
-foreign import hashString :: String -> Hash String
+foreign import hashString :: Int -> String -> Hash String
 
 instance hashableString :: Hashable String where
-  hash = hashString
+  hashWithSalt = hashString
 
-foreign import hashArray :: forall a. (a -> Hash a) -> Array a -> Hash (Array a)
+foreign import hashArray :: forall a. (a -> Hash a) -> Int -> Array a -> Hash (Array a)
 
 instance hashableArray :: Hashable a => Hashable (Array a) where
-  hash = hashArray hash
+  hashWithSalt = hashArray hash
 
 instance hashableUnit :: Hashable Unit where
-  hash _ = Hash 1
+  hashWithSalt s _ = Hash (s + 1)
 
 instance hashableVoid :: Hashable Void where
-  hash _ = Hash 0
+  hashWithSalt s _ = Hash s
 
 instance hashableOrdering :: Hashable Ordering where
-  hash LT = Hash (-1)
-  hash GT = Hash 1
-  hash EQ = Hash 0
+  hashWithSalt s LT = Hash (s - 1)
+  hashWithSalt s GT = Hash (s + 1)
+  hashWithSalt s EQ = Hash (s + 0)
 
 class EqRecord l r <= HashableRecord l r | l -> r where
-  hashRecord :: RLProxy l -> Record r -> Hash (Record r)
+  hashRecord :: Int -> RLProxy l -> Record r -> Hash (Record r)
 
 instance hashableRecordNil :: HashableRecord Nil r where
-  hashRecord _ _ = Hash 0
+  hashRecord s _ _ = Hash s
 
 instance hashableRecordCons ::
   ( Hashable vt
@@ -104,15 +110,14 @@ instance hashableRecordCons ::
   , IsSymbol l
   , Row.Cons l vt whatev r
   ) => HashableRecord (Cons l vt tl) r where
-  hashRecord rlp record =
-    let (Hash rHash) = hashRecord (RLProxy :: RLProxy tl) record
+  hashRecord s rlp record =
+    let (Hash rHash) = hashRecord s (RLProxy :: RLProxy tl) record
         field :: vt
         field = unsafeGet (reflectSymbol (SProxy :: SProxy l)) record
-        (Hash fHash) = hash field
-    -- this mimics Java's hash function for arrays
+        (Hash fHash) = hashWithSalt s field
     in Hash (rHash * 31 + fHash)
 
 instance hashableRecord ::
   (RowToList r l, HashableRecord l r, EqRecord l r)
   => Hashable (Record r) where
-  hash = hashRecord (RLProxy :: RLProxy l)
+  hashWithSalt s = hashRecord s (RLProxy :: RLProxy l)
