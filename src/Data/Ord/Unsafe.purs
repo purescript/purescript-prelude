d84c89f478f2e9b4a13847b236f0e4cd4a5783b9
Andy Arvanitis <andyarvanitis@gmail.com>
2019-04-14 12:04:09 -0700

    Add deprecation warning to unsafeCompare

--- src/Data/Ord/Unsafe.purs
+++ src/Data/Ord/Unsafe.purs
@@ -1,8 +1,12 @@
 module Data.Ord.Unsafe (unsafeCompare) where
 
+import Prim.TypeError (class Warn, Text)
 import Data.Ordering (Ordering(..))
 
-unsafeCompare :: forall a. a -> a -> Ordering
+unsafeCompare
+    :: forall a.
+       Warn (Text "'unsafeCompare' is deprecated.")
+    => a -> a -> Ordering
 unsafeCompare = unsafeCompareImpl LT EQ GT
 
 foreign import unsafeCompareImpl
