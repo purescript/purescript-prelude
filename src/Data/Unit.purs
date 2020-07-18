c9d70c30e12a5cbc5127a716f4cd5489e0c2cdcb
milesfrain <milesfrain@users.noreply.github.com>
2020-07-18 10:14:56 -0700

    Beginner-friendly notes on Unit

--- src/Data/Unit.purs
+++ src/Data/Unit.purs
@@ -7,9 +7,15 @@ import Data.Show (class Show)
 -- |
 -- | `Unit` is often used, wrapped in a monadic type constructor, as the
 -- | return type of a computation where only the _effects_ are important.
+-- |
+-- | You can think of `Unit` as being analogous to the `void` keyword in
+-- | other languages, such as C, Java, etc., which indicates that the
+-- | function does not return any _meaningful_ data.
 foreign import data Unit :: Type
 
 -- | `unit` is the sole inhabitant of the `Unit` type.
+-- |
+-- | Use this to create a _value_ of the `Unit` type.
 foreign import unit :: Unit
 
 instance showUnit :: Show Unit where
