f2f7ca82522a154a3eb0e5841dbc3b144b6a1015
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-12-23 09:51:16 -0800

    Remove Generic Enum from src and test

--- test/Main.purs
+++ test/Main.purs
@@ -2,10 +2,8 @@ module Test.Main where
 
 import Prelude
 
-import Data.Enum (class BoundedEnum, class Enum, Cardinality(..), cardinality, fromEnum, pred, succ, toEnum, enumFromTo)
 import Data.Generic.Rep as G
 import Data.Generic.Rep.Bounded as GBounded
-import Data.Generic.Rep.Enum as GEnum
 import Data.Generic.Rep.Eq as GEq
 import Data.Generic.Rep.HeytingAlgebra as GHeytingAlgebra
 import Data.Generic.Rep.Ord as GOrd
@@ -43,13 +41,6 @@ instance showSimpleBounded :: Show SimpleBounded where
 instance boundedSimpleBounded :: Bounded SimpleBounded where
   bottom = GBounded.genericBottom
   top = GBounded.genericTop
-instance enumSimpleBounded :: Enum SimpleBounded where
-  pred = GEnum.genericPred
-  succ = GEnum.genericSucc
-instance boundedEnumSimpleBounded :: BoundedEnum SimpleBounded where
-  cardinality = GEnum.genericCardinality
-  toEnum = GEnum.genericToEnum
-  fromEnum = GEnum.genericFromEnum
 
 data Option a = None | Some a
 derive instance genericOption :: G.Generic (Option a) _
@@ -62,13 +53,6 @@ instance showOption :: Show a => Show (Option a) where
 instance boundedOption :: Bounded a => Bounded (Option a) where
   bottom = GBounded.genericBottom
   top = GBounded.genericTop
-instance enumOption :: (Bounded a, Enum a) => Enum (Option a) where
-  pred = GEnum.genericPred
-  succ = GEnum.genericSucc
-instance boundedEnumOption :: BoundedEnum a => BoundedEnum (Option a) where
-  cardinality = GEnum.genericCardinality
-  toEnum = GEnum.genericToEnum
-  fromEnum = GEnum.genericFromEnum
 
 data Bit = Zero | One
 derive instance genericBit :: G.Generic Bit _
@@ -81,13 +65,6 @@ instance showBit :: Show Bit where
 instance boundedBit :: Bounded Bit where
   bottom = GBounded.genericBottom
   top = GBounded.genericTop
-instance enumBit :: Enum Bit where
-  pred = GEnum.genericPred
-  succ = GEnum.genericSucc
-instance boundedEnumBit :: BoundedEnum Bit where
-  cardinality = GEnum.genericCardinality
-  toEnum = GEnum.genericToEnum
-  fromEnum = GEnum.genericFromEnum
 
 data Pair a b = Pair a b
 derive instance genericPair :: G.Generic (Pair a b) _
@@ -100,13 +77,6 @@ instance showPair :: (Show a, Show b) => Show (Pair a b) where
 instance boundedPair :: (Bounded a, Bounded b) => Bounded (Pair a b) where
   bottom = GBounded.genericBottom
   top = GBounded.genericTop
-instance enumPair :: (Bounded a, Enum a, Bounded b, Enum b) => Enum (Pair a b) where
-  pred = GEnum.genericPred
-  succ = GEnum.genericSucc
-instance boundedEnumPair :: (BoundedEnum a, BoundedEnum b) => BoundedEnum (Pair a b) where
-  cardinality = GEnum.genericCardinality
-  toEnum = GEnum.genericToEnum
-  fromEnum = GEnum.genericFromEnum
 
 data A1 = A1 (Tuple (Tuple Int {a :: Int}) {a :: Int})
 derive instance genericA1 :: G.Generic A1 _
@@ -174,63 +144,6 @@ main = do
 
   log "Checking product top"
   assert $ top == (Pair One D :: Pair Bit SimpleBounded)
-
-  log "Checking simple pred bottom"
-  assert $ pred (bottom :: SimpleBounded) == Nothing
-
-  log "Checking simple (pred =<< succ bottom)"
-  assert $ (pred =<< succ bottom) == Just A
-
-  log "Checking simple succ top"
-  assert $ succ (top :: SimpleBounded) == Nothing
-
-  log "Checking simple (succ =<< pred top)"
-  assert $ (succ =<< pred top) == Just D
-
-  log "Checking composite pred bottom"
-  assert $ pred (bottom :: Option SimpleBounded) == Nothing
-
-  log "Checking composite (pred =<< succ bottom)"
-  assert $ (pred =<< succ (bottom :: Option SimpleBounded)) == Just None
-
-  log "Checking composite succ top"
-  assert $ succ (top :: Option SimpleBounded) == Nothing
-
-  log "Checking composite (succ =<< pred top)"
-  assert $ (succ =<< pred top) == Just (Some D)
-
-  log "Checking product pred bottom"
-  assert $ pred (bottom :: Pair Bit SimpleBounded) == Nothing
-
-  log "Checking product (pred =<< succ bottom)"
-  assert $ (pred =<< succ (bottom :: Pair Bit SimpleBounded)) == Just (Pair Zero A)
-
-  log "Checking product succ top"
-  assert $ succ (top :: Pair Bit SimpleBounded) == Nothing
-
-  log "Checking product (succ =<< pred top)"
-  assert $ (succ =<< pred top) == Just (Pair One D)
-
-  log "Checking simple cardinality"
-  assert $ (cardinality :: Cardinality SimpleBounded) == Cardinality 4
-
-  log "Checking composite cardinality"
-  assert $ (cardinality :: Cardinality (Option SimpleBounded)) == Cardinality 5
-
-  log "Checking product cardinality"
-  assert $ (cardinality :: Cardinality (Pair Bit SimpleBounded)) == Cardinality 8
-
-  log "Checking simple toEnum/fromEnum roundtrip"
-  assert $ toEnum (fromEnum A) == Just A
-  assert $ toEnum (fromEnum B) == Just B
-
-  log "Checking composite toEnum/fromEnum roundtrip"
-  assert $ toEnum (fromEnum (None :: Option SimpleBounded)) == Just (None :: Option SimpleBounded)
-  assert $ toEnum (fromEnum (Some A)) == Just (Some A)
-
-  log "Checking product toEnum/fromEnum roundtrip"
-  assert $ let allPairs = enumFromTo bottom top :: Array (Pair Bit SimpleBounded)
-           in (toEnum <<< fromEnum <$> allPairs) == (Just <$> allPairs)
 
   log "Checking zero"
   assert $ (zero :: A1) == A1 (Tuple (Tuple 0 {a: 0}) {a: 0})
