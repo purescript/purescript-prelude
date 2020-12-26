a180105e689629deb9263c987948564031527cb7
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-12-25 20:16:04 -0800

    Add Show instance to generic rep types (complete daniel-chambers PR)
    Helps one see the generic representation for a given value

--- test/Data/Generic/Rep.purs
+++ test/Data/Generic/Rep.purs
@@ -123,6 +123,9 @@ testGenericRep = do
   assert "Checking show" $
     show (cons 1 (cons 2 Nil)) == "(Cons { head: 1, tail: (Cons { head: 2, tail: Nil }) })"
 
+  assert "Checking show for generic types" $
+    show (G.from $ cons 1 Nil) == "(Inr (Constructor \"Cons\" (Argument { head: 1, tail: Nil })))"
+
   assert "Checking equality" $
     cons 1 (cons 2 Nil) == cons 1 (cons 2 Nil)
 
