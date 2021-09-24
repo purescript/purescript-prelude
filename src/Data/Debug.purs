6360aacf516ddc5b18c3fc0d17d77b2d9dd46ee8
Jordan Martinez <jordanalex.martinez@protonmail.com>
2021-09-24 08:58:10 -0700

    Delete Data.Debug module
    This enables Data.Debug.Class to take its spot,
    following the naming convention of type classes
    in purescript-prelude

--- src/Data/Debug.purs
+++
@@ -1,9 +0,0 @@
-module Data.Debug
-  ( module Data.Debug.Type
-  , module Data.Debug.Class
-  , module Data.Debug.Generic
-  ) where
-
-import Data.Debug.Type (Repr, array, assoc, boolean, char, collection, constructor, int, number, opaque, opaqueLiteral, opaque_, record, string)
-import Data.Debug.Class (class Debug, class DebugRowList, debug, debugRowList)
-import Data.Debug.Generic (class GenericDebug, class GenericDebugArgs, genericDebug, genericDebug', genericDebugArgs)
