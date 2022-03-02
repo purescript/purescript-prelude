2994f6de50f77f3868267bfee2b1f773e2c7c530
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-02 15:23:31 -0600

    Fix eslint issues

--- test/Test/Main.js
+++ test/Test/Main.js
@@ -16,29 +16,29 @@ export function testNumberShow(showNumber) {
     }
 
     testAll([
-        // Within Int range
-        [0.0, "0.0"],
-        [1.0, "1.0"],
-        [-1.0, "-1.0"],
-        [500.0, "500.0"],
+      // Within Int range
+      [0.0, "0.0"],
+      [1.0, "1.0"],
+      [-1.0, "-1.0"],
+      [500.0, "500.0"],
 
-        // Outside Int range
-        [1e10, "10000000000.0"],
-        [1e10 + 0.5, "10000000000.5"],
-        [-1e10, "-10000000000.0"],
-        [-1e10 - 0.5, "-10000000000.5"],
+      // Outside Int range
+      [1e10, "10000000000.0"],
+      [1e10 + 0.5, "10000000000.5"],
+      [-1e10, "-10000000000.0"],
+      [-1e10 - 0.5, "-10000000000.5"],
 
-        // With exponent
-        [1e21, "1e+21"],
-        [1e-21, "1e-21"],
+      // With exponent
+      [1e21, "1e+21"],
+      [1e-21, "1e-21"],
 
-        // With decimal and exponent
-        [1.5e21, "1.5e+21"],
-        [1.5e-10, "1.5e-10"],
+      // With decimal and exponent
+      [1.5e21, "1.5e+21"],
+      [1.5e-10, "1.5e-10"],
 
-        [NaN, "NaN"],
-        [Infinity, "Infinity"],
-        [-Infinity, "-Infinity"],
-      ]);
+      [NaN, "NaN"],
+      [Infinity, "Infinity"],
+      [-Infinity, "-Infinity"],
+    ]);
   };
 }
