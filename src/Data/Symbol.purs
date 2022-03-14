73a195eb7fdb97370ec67290bbcf9c5092c0cd85
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-14 11:08:58 -0700

    Remove forall proxy workaround comment

--- src/Data/Symbol.purs
+++ src/Data/Symbol.purs
@@ -8,13 +8,6 @@ import Type.Proxy (Proxy(..))
 
 -- | A class for known symbols
 class IsSymbol (sym :: Symbol) where
-  -- Note: Before v0.14.0, we did not have polykinds. Thus, we needed
-  -- kind-specific proxy types to pass a Symbol around.
-  -- Once v0.14.0 was released, we could use the kind-generic `Proxy` type
-  -- instead. However, to reduce code breakage, we're using
-  -- `forall proxy. proxy sym` here so that `SProxy` code will still compile.
-  -- When PureScript makes a new breaking release after the v0.14.0 release,
-  -- this type signature will be updated to `Proxy sym -> String`.
   reflectSymbol :: Proxy sym -> String
 
 -- local definition for use in `reifySymbol`
