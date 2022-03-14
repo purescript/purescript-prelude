31b327406659af17612c926d85f388bb9a848a18
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-14 11:06:27 -0700

    Drop deprecated SProxy

--- src/Data/Symbol.purs
+++ src/Data/Symbol.purs
@@ -2,15 +2,9 @@ module Data.Symbol
   ( class IsSymbol
   , reflectSymbol
   , reifySymbol
-  , SProxy(..)
   ) where
 
 import Type.Proxy (Proxy(..))
-
--- | A value-level proxy for a type-level symbol.
--- | **Deprecated as of v0.14.0 PureScript release**: use `Type.Proxy` instead.
-data SProxy :: Symbol -> Type
-data SProxy sym = SProxy
 
 -- | A class for known symbols
 class IsSymbol (sym :: Symbol) where
@@ -21,14 +15,14 @@ class IsSymbol (sym :: Symbol) where
   -- `forall proxy. proxy sym` here so that `SProxy` code will still compile.
   -- When PureScript makes a new breaking release after the v0.14.0 release,
   -- this type signature will be updated to `Proxy sym -> String`.
-  reflectSymbol :: forall proxy. proxy sym -> String
+  reflectSymbol :: Proxy sym -> String
 
 -- local definition for use in `reifySymbol`
 foreign import unsafeCoerce :: forall a b. a -> b
 
-reifySymbol :: forall proxy r. String -> (forall sym. IsSymbol sym => proxy sym -> r) -> r
+reifySymbol :: forall r. String -> (forall sym. IsSymbol sym => Proxy sym -> r) -> r
 reifySymbol s f = coerce f { reflectSymbol: \_ -> s } Proxy where
   coerce
-    :: (forall sym1. IsSymbol sym1              => proxy sym1 -> r)
+    :: (forall sym1. IsSymbol sym1              => Proxy sym1 -> r)
     -> { reflectSymbol :: Proxy "" -> String } -> Proxy ""   -> r
   coerce = unsafeCoerce
