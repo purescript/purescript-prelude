9888da59c1dcab5949ee7e22f096be7e8697d310
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-16 15:42:54 -0500

    Remove show duplicate label test

--- test/Test/Utils.purs
+++ test/Test/Utils.purs
@@ -1,7 +1,5 @@
 module Test.Utils where
 
-import Prim.Row (class Union)
-
 import Prelude
 
 type AlmostEff = Unit -> Unit
@@ -10,15 +8,3 @@ assert :: String -> Boolean -> AlmostEff
 assert msg condition = if condition then const unit else throwErr msg
 
 foreign import throwErr :: String -> AlmostEff
-
--- Copied from https://github.com/purescript/purescript-record/blob/master/src/Record.purs#L182-L188
--- and renamed to `recordUnion`
-recordUnion
-  :: forall r1 r2 r3
-   . Union r1 r2 r3
-  => Record r1
-  -> Record r2
-  -> Record r3
-recordUnion l r = unsafeUnion l r
-
-foreign import unsafeUnion :: forall r1 r2 r3. Record r1 -> Record r2 -> Record r3
