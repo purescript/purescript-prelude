15b06b6017f5e411c64a039f2cbb2e27bd3b8012
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-04-21 00:10:04 -0500

    Replace showStringImpl with one based on compiler's version

--- src/Data/Show.js
+++ src/Data/Show.js
@@ -24,28 +24,36 @@ export const showCharImpl = function (c) {
   return c === "'" || c === "\\" ? "'\\" + c + "'" : "'" + c + "'";
 };
 
+const showHex = function(width, c) {
+  return Number(c).toString(16).padStart(width, "0");
+};
+
 export const showStringImpl = function (s) {
-  var l = s.length;
-  return "\"" + s.replace(
-    /[\0-\x1F\x7F"\\]/g, // eslint-disable-line no-control-regex
-    function (c, i) {
-      switch (c) {
-        case "\"":
-        case "\\":
-          return "\\" + c;
-        case "\x07": return "\\a";
-        case "\b": return "\\b";
-        case "\f": return "\\f";
-        case "\n": return "\\n";
-        case "\r": return "\\r";
-        case "\t": return "\\t";
-        case "\v": return "\\v";
+  var str = "";
+  Array.from(s, function(str) { return str.charCodeAt(0); })
+    .forEach(function (cp) {
+      if (cp > 0xFF) {
+        str += "\\u" + showHex(4, cp);
+      } else if (cp > 0x7E || cp < 0x20) {
+        str += "\\x" + showHex(2, cp);
+      } else {
+        const ch = String.fromCodePoint(cp);
+        switch (ch) {
+          case "\b": str += "\\b"; break;
+          case "\t": str += "\\t"; break;
+          case "\n": str += "\\n"; break;
+          case "\v": str += "\\v"; break;
+          case "\f": str += "\\f"; break;
+          case "\r": str += "\\r"; break;
+          case "\"":
+          case "\\":
+            str += "\\" + ch;  break;
+          default:
+            str += ch;  break;
+        }
       }
-      var k = i + 1;
-      var empty = k < l && s[k] >= "0" && s[k] <= "9" ? "\\&" : "";
-      return "\\" + c.charCodeAt(0).toString(10) + empty;
-    }
-  ) + "\"";
+    });
+  return "\"" + str + "\"";
 };
 
 export const showArrayImpl = function (f) {
