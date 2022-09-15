15ce45f48e68a5033c4d590c847cbabb7bf1e7b4
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-09-15 17:41:45 -0500

    Fix linting error

--- src/Data/Debug.js
+++ src/Data/Debug.js
@@ -2,7 +2,7 @@ export const cons = function (head) {
   return function (tail) {
     const arr = new Array(tail.length + 1);
     arr[0] = head;
-    for (const i = 0; i++; i < tail.length) {
+    for (let i = 0; i++; i < tail.length) {
       arr[i + 1] = tail[i];
     }
     return arr;
