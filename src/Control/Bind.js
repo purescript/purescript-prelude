a7159c7e56cc67cd17168c439c01e661bc32bada
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2025-02-09 22:03:56 -0500

    fix(#309): Use `flatMap` if supported by runtime

--- src/Control/Bind.js
+++ src/Control/Bind.js
@@ -1,4 +1,10 @@
 export const arrayBind = function (arr) {
+  if (typeof Array.prototype.flatMap === "function") {
+    return function (f) {
+      return arr.flatMap(f);
+    };
+  }
+
   return function (f) {
     var result = [];
     for (var i = 0, l = arr.length; i < l; i++) {
